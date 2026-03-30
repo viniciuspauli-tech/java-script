b6a65b80bf1aec8cc9049dd5b08e0757701c6775
Vinicius Pauli <vinicius_pauli@estudante.sesisenai.org.br>
2026-03-30 11:27:00 -0300

    Add salary calculation function with INSS discounts

--- script/ex11.js
+++ script/ex11.js
@@ -1,1 +1,17 @@
+function folha(){
+    let salario = parseFloat(prompt("informe seu salario: "));
+    let liquido;
 
+    if(salario <= 1000){
+        liquido = salario - (salario*0.08);
+        alert("Seu salario era: " + salario + ", com um desconto de 8% do inss, o se salario liquido é de: " + liquido);
+    } else if (salario >= 1000.01 & salario <= 1500){
+        liquido = salario - (salario*0.085);
+        alert("Seu salario era: " + salario + ", com um desconto de 8.5% do inss, o se salario liquido é de: " + liquido);
+    } else if(salario > 1500) {
+          liquido = salario - (salario*0.09);
+        alert("Seu salario era: " + salario + ", com um desconto de 9% do inss, o se salario liquido é de: " + liquido);
+    } else {
+        alert("salario inválido")
+    }
+}
