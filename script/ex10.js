0f39a9935fd876049f021fe20a35f43b1b54c7df
Vinicius Pauli <vinicius_pauli@estudante.sesisenai.org.br>
2026-03-30 11:26:09 -0300

    Add pagamento function to calculate net salary

--- script/ex10.js
+++ script/ex10.js
@@ -1,1 +1,8 @@
+function pagamento(){
+    let nome = prompt("informe seu nome: ");
+    let salario = parseFloat(prompt("informe seu salario bruto: "));
+    let liquido = salario - (salario*0.08);
 
+    alert("Olá, "+ nome +" seu salário bruto é: "+ salario);
+    alert("Com o desconto do inss de 8%, seu salario liquido é: " + liquido);
+}
