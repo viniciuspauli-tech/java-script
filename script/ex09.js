6f79460c7e858607ff07e8f8e72050839396ba3a
Vinicius Pauli <vinicius_pauli@estudante.sesisenai.org.br>
2026-03-30 11:24:54 -0300

    Add function to determine and log larger number

--- script/ex09.js
+++ script/ex09.js
@@ -1,1 +1,16 @@
+function ordenacao(){
+    let a = 5;
+    let b = 10;
+    let maior;
+    let menor;
 
+    if (a > b){
+        maior = a;
+        menor = b;
+    } else {
+        maior = b;
+        menor = a;
+    }
+
+    console.log("O número maior é: " + maior + ", o número menor é: "+ menor);
+}
