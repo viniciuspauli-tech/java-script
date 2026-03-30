24fd1c9350b2e2c87e8a9d977ecd640b5fc3e1eb
Vinicius Pauli <vinicius_pauli@estudante.sesisenai.org.br>
2026-03-30 11:24:09 -0300

    Add operacao function for basic arithmetic operations

--- script/ex08.js
+++ script/ex08.js
@@ -1,1 +1,16 @@
+function operacao(){
+    let a = 4;
+    let b = 2;
+    let diferenca;
+    let dobro;
+    let multi;
 
+    diferenca = a - b;
+    dobro = (a*2) + (b*3);
+    multi = a * b;
+
+    alert("usando a = 4 e b = 2:");
+    alert("A diferença entre as duas variáveis: " + diferenca);
+    alert("O dobro da primeira variável somado ao triplo da segunda: "+ dobro);
+    alert("A multiplicação das duas variáveis: "+ multi);
+}
