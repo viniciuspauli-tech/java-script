5b3fd858574b27f5d6b4b12b0519ef62229dfbd0
Vinicius Pauli <vinicius_pauli@estudante.sesisenai.org.br>
2026-03-30 11:21:55 -0300

    Replace ice cream flavors with new options

--- script/ex07.js
+++ script/ex07.js
@@ -3,48 +3,48 @@ function cardapio() {
 
     switch (escolha) {
         case "a":
-        case "chocolate":
-            alert("sabor chocolate, valor R$1,50");
+        case "sopa":
+            alert("sabor sopa, valor R$1,50");
             break;
 
         case "b":
-        case "morango":
-            alert("sabor morango, valor R$2,50");
+        case "brocolis":
+            alert("sabor brocolis, valor R$2,50");
             break;
 
         case "c":
-        case "creme":
-            alert("sabor creme, valor R$2,50");
+        case "baunilha":
+            alert("sabor baunilha, valor R$2,50");
             break;
 
         case "d":
-        case "manga":
-            alert("sabor manga, valor R$3,20");
+        case "pessego":
+            alert("sabor pessego, valor R$3,20");
             break;
 
         case "e":
-        case "melancia":
-            alert("sabor melancia, valor R$3,40");
+        case "abacate":
+            alert("sabor abacate, valor R$3,40");
             break;
 
         case "f":
-        case "vanilla ice":
-            alert("sabor vanilla ice, valor R$3,00");
+        case "floresta negra":
+            alert("sabor floresta negra, valor R$3,00");
             break;
 
         case "g":
-        case "ceu azul":
-            alert("sabor ceu azul, valor R$3,60");
+        case "laranja":
+            alert("sabor laranja, valor R$3,60");
             break;
 
         case "h":
-        case "brownie":
-            alert("sabor brownie, valor R$4,00");
+        case "morango":
+            alert("sabor morango, valor R$4,00");
             break;
 
         case "i":
-        case "hawaiano":
-            alert("sabor hawaiano, valor R$5,00");
+        case "maracuja":
+            alert("sabor maracuja, valor R$5,00");
             break;
 
         default:
